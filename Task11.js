acc33cca13daa789ef954313c609caf5a384b694
Abhishek <abhibaba007@gmail.com>
2022-07-15 19:38:49 +0530

    fat Arrow

--- Task11.js
+++ Task11.js
@@ -1,42 +1,34 @@
 class student{
     static count=0;
+    // let minAge=15;
+    // let minMarks=40;
     constructor(name,age,phoneno,marks)
     {
         this.name=name;
         this.age=age;
         this.phoneno=phoneno;
         this.marks=marks;
-           student.countStu();
-       
-    }
-    static countStu(){
-        return student.count++;
+                
     }
     
-    eligible()
-{
-    if(this.marks>40)
-    {
-        console.log(`${this.name} is Eligible`);
-    }
-    else
-    {
-        console.log(`${this.name} is not Eligible`);
-    }
-}
-}
 
+    placeElig(minAge=15)
+    {
+        return(minMarks=40)=>{
+        if(this.marks>minMarks && this.age >minAge)
+        {
+            console.log(`${this.name} is eligible for placement`)
+        }
+    }
+    }
+    
+}
 
 const student1=new student('Rahul',15,4567,42);
 const student2=new student('Sameer',16,674567,38);
 const student3=new student('Sanjeev',15,456987,47);
 const student4=new student('Ram',15,6544567,67);
 const student5=new student('Shyam',16,4523467,25);
-console.log(student.countStu());
-student1.eligible();
-student2.eligible();
-student3.eligible();
-student4.eligible();
-student5.eligible();
 
+student1.placeElig();
 
